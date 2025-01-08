e182a63fa21f053dc4e9369090870680547e33b5
Ivaylo1992 <ivailop123@gmail.com>
2025-01-08 05:36:25 +0200

    added more solutions

---
+++ JS Basics/lab/first_steps_in_coding/yardGreening.js
@@ -0,0 +1,11 @@
+function yardGreening(squareMeters){
+    let pricePerSquareMeter = 7.61
+    let totalPrice = Number(squareMeters) * pricePerSquareMeter
+    let discount = totalPrice * 0.18
+    let finalPrice = totalPrice - discount
+
+    console.log(`The final price is: ${finalPrice} lv.`)
+    console.log(`The discount is: ${discount} lv.`)
+}
+
+yardGreening('150')
