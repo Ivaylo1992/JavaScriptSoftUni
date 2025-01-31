9d6621822a5d2943c81cf36d8ec2a2e2a4b79c83
Ivaylo1992 <ivailop123@gmail.com>
2025-01-31 06:51:30 +0200

    added more solutions

--- JSBasics/exercises/first-steps-in-coding/depositCalculator.js
+++ JSBasics/exercises/first-steps-in-coding/depositCalculator.js
@@ -3,5 +3,3 @@ function depositCalculator(depositSum, depositPeriod, anualInterest){
     let finalSum = depositSum + depositPeriod * ((depositSum * newAnualInterest) / 12)
     console.log(finalSum)
 }
-
-depositCalculator(200, 3, 5.7)
