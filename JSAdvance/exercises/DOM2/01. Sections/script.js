d1a15bb98d853774b9cdf07807f6001591a9e511
Ivaylo1992 <ivailop123@gmail.com>
2025-04-25 06:34:53 -0700

    DOM 2 exercise solutions

--- JSAdvance/exercises/DOM2/01. Sections/script.js
+++ JSAdvance/exercises/DOM2/01. Sections/script.js
@@ -1,3 +1,22 @@
 function create(words) {
-   console.log('TODO:...');
+   const contentDiv = document.getElementById('content')
+
+   const div = document.createElement('div');
+   const p = document.createElement('p');
+
+   for (let el of words) {
+      const div = document.createElement('div');
+      const p = document.createElement('p');
+      p.textContent = el;
+      p.style.display = 'none';
+
+      div.appendChild(p);
+      div.addEventListener('click', () => {
+         p.style.display = 'block';
+      });
+
+      contentDiv.appendChild(div);
+   }
+   
+   
 }
--- JSAdvance/exercises/DOM2/02. Time-Converter/solution.js
+++ JSAdvance/exercises/DOM2/02. Time-Converter/solution.js
@@ -1,4 +1,29 @@
 function attachEventsListeners() {
+    const inputElements = {
+        days: document.getElementById('days'),
+        hours: document.getElementById('hours'),
+        minutes: document.getElementById('minutes'),
+        seconds: document.getElementById('seconds')
+    };
 
-    console.log('TODO:...');
+    const ratios = {
+        days: 1,
+        hours: 24,
+        minutes: 1440,
+        seconds: 86400
+    };
+
+    document.getElementById('daysBtn').addEventListener('click', () => convert('days'));
+    document.getElementById('hoursBtn').addEventListener('click', () => convert('hours'));
+    document.getElementById('minutesBtn').addEventListener('click', () => convert('minutes'));
+    document.getElementById('secondsBtn').addEventListener('click', () => convert('seconds'));
+
+    function convert(unit) {
+        const value = Number(inputElements[unit].value);
+        const days = value / ratios[unit]; // normalize to days
+
+        for (const key in inputElements) {
+            inputElements[key].value = days * ratios[key];
+        }
+    }
 }
