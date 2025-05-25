f3aada3af7f4951bbe0b5d49f5f6eb4bc1cc8590
Ivaylo1992 <ivailop123@gmail.com>
2025-05-24 21:48:04 -0700

    02.Bus-Schedule exercise solution

--- JSApplications/exercises/asynchronous-programming/02.Bus-Schedule/app.js
+++ JSApplications/exercises/asynchronous-programming/02.Bus-Schedule/app.js
@@ -1,17 +1,41 @@
 function solve() {
+  const departButton = document.querySelector("#depart");
+  const arriveButton = document.querySelector("#arrive");
+  const infoSpan = document.querySelector("span");
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+  let currentStopId = "depot";
+  let nextStopId = "";
+  let currentStopName = "";
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+  function depart() {
+    const url = `http://localhost:3030/jsonstore/bus/schedule/${currentStopId}`;
+    fetch(url)
+      .then((result) => result.json())
+      .then((info) => {
+        currentStopName = info.name;
+        nextStopId = info.next;
+        infoSpan.textContent = `Next stop ${currentStopName}`;
+        arriveButton.removeAttribute("disabled");
+        departButton.setAttribute("disabled", true);
+      })
+      .catch((err) => {
+        arriveButton.setAttribute("disabled", true);
+        departButton.setAttribute("disabled", true);
+      });
+  }
 
-    return {
-        depart,
-        arrive
-    };
+  function arrive() {
+    infoSpan.textContent = `Arriving at ${currentStopName}`;
+    currentStopId = nextStopId;
+
+    departButton.removeAttribute("disabled");
+    arriveButton.setAttribute("disabled", true);
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
