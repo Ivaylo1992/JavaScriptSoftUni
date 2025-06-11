aa64d1755f3bf36cee19c5974145306d8267ba3c
Ivaylo1992 <ivailop123@gmail.com>
2025-06-11 05:53:27 +0300

    03.Students task solution

--- JSApplications/exercises/remote-data-and-authentications/03.Students/app.js
+++ JSApplications/exercises/remote-data-and-authentications/03.Students/app.js
@@ -1,1 +1,56 @@
-console.log('TODO...');
+const url = "http://localhost:3030/jsonstore/collections/students";
+const tableBody = document.querySelector("tbody");
+
+async function renderStudentsList() {
+  tableBody.innerHTML = "";
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    for (const { firstName, lastName, facultyNumber, grade } of Object.values(
+      data
+    )) {
+      const newRow = document.createElement("tr");
+      tableBody.appendChild(newRow);
+
+      newRow.innerHTML = `
+          <td>${firstName}</td>
+          <td>${lastName}</td>
+          <td>${facultyNumber}</td>
+          <td>${Number(grade).toFixed(2)}</td>
+      `;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+renderStudentsList();
+
+const formEl = document.querySelector("form");
+formEl.addEventListener("submit", handleForm);
+
+async function handleForm(e) {
+  e.preventDefault();
+  const formData = new FormData(formEl);
+
+  const formEntries = Object.fromEntries(formData);
+
+  for (const entry of Object.values(formEntries)) {
+    if (!entry) {
+      return;
+    }
+  }
+
+  try {
+    await fetch(url, {
+      method: "POST",
+      "Content-Type": "application/json",
+      body: JSON.stringify(formEntries),
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
+  renderStudentsList();
+}
