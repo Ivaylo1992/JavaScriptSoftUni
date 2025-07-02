eb50a63343d1e45aeef6d91907dcc12be794b95e
Ivaylo1992 <ivailop123@gmail.com>
2025-07-02 06:51:08 +0300

    new solution

--- JSApplications/exercises/single-page-application/04.JS-Applications-Single-Page-Applications-Exercise-Resources/01.Forum/src/app.js
+++ JSApplications/exercises/single-page-application/04.JS-Applications-Single-Page-Applications-Exercise-Resources/01.Forum/src/app.js
@@ -1,9 +1,1 @@
-import { handleCreate } from "./createTopic.js";
-
-const postsUrl = "http://localhost:3030/jsonstore/collections/myboard/posts";
-const createForm = document.querySelector("main form");
-
-createForm.addEventListener(
-  "submit",
-  handleCreate.bind(null, postsUrl, createForm)
-);
+import { createTopic } from "./createTopic.js";
