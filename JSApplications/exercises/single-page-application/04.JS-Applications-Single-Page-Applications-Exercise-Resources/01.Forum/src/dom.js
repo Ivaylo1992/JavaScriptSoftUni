b4b7212f336aef7e0e4ed67661ea1017e3a98562
Ivaylo1992 <ivailop123@gmail.com>
2025-07-07 21:16:45 +0300

    feat: added createTopicHTML function

--- JSApplications/exercises/single-page-application/04.JS-Applications-Single-Page-Applications-Exercise-Resources/01.Forum/src/dom.js
+++ JSApplications/exercises/single-page-application/04.JS-Applications-Single-Page-Applications-Exercise-Resources/01.Forum/src/dom.js
@@ -1,3 +1,26 @@
+const topics = document.querySelector(".topic-container");
+
+export function createTopicHTML(data) {
+  const { title, username, post, date, _id } = data;
+  const topic = htmlGenerator("div", "", "topic-name-wraper", topics);
+  topic.id = _id;
+
+  const divTopicName = htmlGenerator("div", "", "topic-name", topic);
+  const normalA = htmlGenerator("a", "", "normal", divTopicName);
+  normalA.setAttribute("href", "#");
+  htmlGenerator("h2", title, "", normalA);
+
+  const divColumns = htmlGenerator("div", "", "columns", divTopicName);
+  const divOne = htmlGenerator("div", "", "", divColumns);
+  const pTag = htmlGenerator("p", "Date", "", divOne);
+  htmlGenerator("time", date, "", pTag);
+
+  const divNickname = htmlGenerator("div", "", "nick-name", divOne);
+  const pUsername = htmlGenerator("p", "Username: ", "", divNickname);
+  htmlGenerator("span", username, "", pUsername);
+  return topic;
+}
+
 function htmlGenerator(type, text, className, parent) {
   const element = document.createElement(type);
   element.textContent = text;
