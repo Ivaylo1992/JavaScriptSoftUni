67a94154120e45d7bc7ca5e9827c5e7381826168
Ivaylo1992 <ivailop123@gmail.com>
2025-07-16 06:29:36 +0300

    test the acordion app

--- JSApplications/lab/architecture-and-testing/01.Accordion/test/accordion.spec.js
+++ JSApplications/lab/architecture-and-testing/01.Accordion/test/accordion.spec.js
@@ -1,22 +1,53 @@
+import { assert, expect } from "chai";
 import { chromium } from "playwright-chromium";
 
-let browser, page;
+let browser, page, buttonID;
 
 describe("Test", async () => {
   before(async () => {
-    browser = await chromium.launch();
+    browser = await chromium.launch({ headless: false, slowMo: 500 });
+    buttonID = "ee9823ab-c3e8-4a14-b998-8c22ec246bd3";
   });
   after(async () => {
-    browser.close();
+    await browser.close();
   });
 
   beforeEach(async () => {
-    page = browser.newPage;
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
   });
 
   afterEach(async () => {
     await page.close();
   });
 
-  it("should be test", () => {});
+  it("should show the extra div when button is clicked", async () => {
+    const isNotDivVisible = await page.isVisible(".extra");
+    await page.click(`#${buttonID}`);
+
+    const isDivVisible = await page.isVisible(".extra");
+
+    assert.isTrue(isDivVisible);
+    assert.isFalse(isNotDivVisible);
+  });
+  it("should change the button text when clicked", async () => {
+    const moreButtonText = await page.textContent(`#${buttonID}`);
+
+    await page.click(`#${buttonID}`);
+
+    const lessButtonText = await page.textContent(`#${buttonID}`);
+
+    assert.equal(moreButtonText, "More");
+    assert.equal(lessButtonText, "Less");
+  });
+  it("should should expexted text in the p tag", async () => {
+    const expectedText = `Scalable Vector Graphics (SVG)`;
+
+    await page.click(`#${buttonID}`);
+
+    let pTagText = await page.textContent("p");
+    pTagText = pTagText.slice(0, 30);
+
+    assert.equal(pTagText, expectedText);
+  });
 });
